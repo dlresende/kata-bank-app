35f57c7731f69c1b4cfedccb3dd0cb055aa957c3
Diego Lemos <diegolemosderesende@gmail.com>
2014-10-30 21:34:07 +0100

    Add cucumber-jvm.

---
+++ src/test/java/BackTest.java
@@ -0,0 +1,10 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/features/"
+)
+public class BackTest {
+}
