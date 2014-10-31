bf29d510bbd096a4ae224a1e25f9f5989294fc3f
Diego Lemos <externe.diego-lemos@tessi.fr>
2014-10-31 15:59:16 +0100

    Isolating bdd tests in a maven profile.

--- src/bdd/java/BackIT.java
+++ src/bdd/java/BackIT.java
@@ -1,12 +1,8 @@
-/*
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features="src/test/features/"
-)
-public class BackTest {
+@CucumberOptions(features="src/bdd/features/")
+public class BackIT {
 }
-*/
