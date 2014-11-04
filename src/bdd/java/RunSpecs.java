f64a7b93a696d6d5ee7f8ee838b10d1121df0d17
Diego Lemos <diegolemosderesende@gmail.com>
2014-11-04 21:17:24 +0100

    Generate cucumber reports.

--- src/bdd/java/RunSpecs.java
+++ src/bdd/java/RunSpecs.java
@@ -3,6 +3,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/bdd/features/")
+@CucumberOptions(
+        features="src/bdd/features/",
+        format = {
+                "pretty",
+                "html:target/cucumber"
+        })
 public class RunSpecs {
 }
