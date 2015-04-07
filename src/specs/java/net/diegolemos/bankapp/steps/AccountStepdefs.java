8ce5a98dcff7ff6b7e74e77985a4195befadafd1
Diego Lemos <diegolemosderesende@gmail.com>
2015-04-07 23:27:08 +0200

    Using mock for acceptance tests.

--- src/specs/java/net/diegolemos/bankapp/steps/AccountStepdefs.java
+++ src/specs/java/net/diegolemos/bankapp/steps/AccountStepdefs.java
@@ -1,57 +1,64 @@
 package net.diegolemos.bankapp.steps;
 
-import static javax.ws.rs.client.Entity.json;
-import static net.diegolemos.bankapp.client.ClientBuilder.aClient;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import javax.ws.rs.client.WebTarget;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.diegolemos.bankapp.account.Account;
+import net.diegolemos.bankapp.account.AccountRepository;
+import net.diegolemos.bankapp.account.AccountService;
 import net.diegolemos.bankapp.client.Client;
+import net.diegolemos.bankapp.client.ClientRepository;
 
-public class AccountStepdefs extends AbstractStepdefs {
+import static net.diegolemos.bankapp.client.ClientBuilder.aClient;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
-    private final WebTarget clientResource = resource("client");
-    private final WebTarget accountResource = resource("account");
+public class AccountStepdefs {
+
+    private ClientRepository clients;
+    private AccountRepository accounts;
+    private AccountService accountService;
+    private Account clientAccount;
+
+    @Before
+    public void setUp() {
+        clients = mock(ClientRepository.class);
+        accounts = mock(AccountRepository.class);
+        accountService = new AccountService(accounts, clients);
+    }
 
     private Client client;
 
     @Given("^an existing client named \"([^\"]*)\"$")
     public void an_existing_client_named(String username) throws Throwable {
         client = aClient().withUsername(username).build();
-        clientResource.path(username).request().put(json(client));
-    }
-
-    @Given("^an existing client named \"([^\"]*)\" with ([^\"]*) EUR in (?:her|his) account$")
-    public void an_existing_client_named_with_EUR_in_her_account(String username, double balance) throws Throwable {
-        an_existing_client_named(username);
-        he_opens_a_bank_account();
-        she_deposits_EUR_into_her_account(balance);
-        the_balance_is_EUR(balance);
     }
 
     @When("^(?:she|he) opens a bank account$")
     public void he_opens_a_bank_account() throws Throwable {
-        Account userAccount = new Account(client);
-        accountResource.request().put(json(userAccount));
-    }
-
-    @When("^(?:she|he) deposits ([^\"]*) EUR into (?:her|his) account$")
-    public void she_deposits_EUR_into_her_account(double amount) throws Throwable {
-        Account account = accountResource.path(client.username()).request().get(Account.class);
-        try {
-            account.deposit(amount);
-            accountResource.request().put(json(account));
-        } catch (IllegalStateException ignored) {
-        }
+        clientAccount = new Account(client);
+        accountService.save(clientAccount);
     }
 
     @Then("^the (?:new|initial) balance is ([^\"]*) EUR$")
     public void the_balance_is_EUR(double expectedBalance) throws Throwable {
-        Account account = accountResource.path(client.username()).request().get(Account.class);
-        assertThat(account.balance(), is(expectedBalance));
+        assertThat(clientAccount.balance(), is(expectedBalance));
     }
 
+    @Given("^an existing client named \"([^\"]*)\" with ([^\"]*) EUR in (?:her|his) account$")
+    public void an_existing_client_named_with_EUR_in_her_account(String username, double balance) throws Throwable {
+        client = aClient().withUsername(username).build();
+        clientAccount = new Account(client);
+        clientAccount.deposit(balance);
+    }
+
+    @When("^(?:she|he) deposits ([^\"]*) EUR into (?:her|his) account$")
+    public void she_deposits_EUR_into_her_account(double amount) throws Throwable {
+        try {
+            clientAccount.deposit(amount);
+        } catch (IllegalStateException ignored) {
+        }
+    }
 }
--- src/specs/java/net/diegolemos/bankapp/steps/ClientStepdefs.java
+++ src/specs/java/net/diegolemos/bankapp/steps/ClientStepdefs.java
@@ -1,23 +1,31 @@
 package net.diegolemos.bankapp.steps;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.diegolemos.bankapp.client.Client;
+import net.diegolemos.bankapp.client.ClientRepository;
+import net.diegolemos.bankapp.client.ClientService;
 
-import javax.ws.rs.client.WebTarget;
 import java.util.Date;
 import java.util.List;
 
-import static javax.ws.rs.client.Entity.json;
 import static jersey.repackaged.com.google.common.collect.Iterables.getOnlyElement;
 import static net.diegolemos.bankapp.client.ClientBuilder.aClient;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-public class ClientStepdefs extends AbstractStepdefs {
+public class ClientStepdefs {
 
-    private final WebTarget clientResource = resource("client");
+    private ClientRepository clients;
+    private ClientService clientService;
+
+    @Before
+    public void setUp() {
+        clients = mock(ClientRepository.class);
+        clientService = new ClientService(clients);
+    }
 
     private Client client;
 
@@ -28,20 +36,13 @@ public class ClientStepdefs extends AbstractStepdefs {
 
     @When("^he becomes a client$")
     public void he_becomes_a_client() throws Throwable {
-        clientResource.path(client.username()).request().put(json(client));
-    }
-
-    @Then("^the following client information should be stored in the system:$")
-    public void the_following_client_information_should_be_stored_in_the_system(List<Client> expectedClients) throws Throwable {
-        Client expectedClient = getOnlyElement(expectedClients);
-        Client existingClient = clientResource.path(client.username()).request().get(Client.class);
-        assertThat(existingClient, equalTo(expectedClient));
+        clientService.save(client);
     }
 
     @Then("^the following information should be stored in the system:$")
     public void the_following_information_should_be_stored_in_the_system(List<Client> expectedClients) throws Throwable {
         Client expectedClient = getOnlyElement(expectedClients);
-        Client existingClient = clientResource.path(client.username()).request().get(Client.class);
-        assertThat(existingClient, equalTo(expectedClient));
+
+        verify(clients).add(expectedClient);
     }
 }
