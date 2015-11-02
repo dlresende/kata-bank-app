2e0e8b0b9e1b3a9594e2c4b7ddb6c675d0944acb
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-11-02 11:31:58 +0100

    Add a scenario which tests deposit from UI

--- src/main/java/net/diegolemos/bankapp/MyServer.java
+++ src/main/java/net/diegolemos/bankapp/MyServer.java
@@ -1,26 +1,34 @@
 package net.diegolemos.bankapp;
 
+import static org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory.createHttpServer;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
-
-import java.io.IOException;
-import java.net.URI;
-
-import static org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory.createHttpServer;
 
 public class MyServer {
     public static final String BANK_APP = "http://localhost:8081/api/";
 
-    public static HttpServer startServer() {
-        return createHttpServer(URI.create(BANK_APP), new BankAppConfig());
+    public static HttpServer startServer() throws MalformedURLException {
+        HttpServer httpServer = createHttpServer(URI.create(BANK_APP), new BankAppConfig());
+        addStaticContent(httpServer);
+        return httpServer;
     }
 
     public static HttpServer startServer(AbstractBinder binder) {
         ResourceConfig config = new ResourceConfig();
         config.packages("net.diegolemos.bankapp");
         config.register(binder);
-        return createHttpServer(URI.create(BANK_APP), config);
+        HttpServer httpServer = createHttpServer(URI.create(BANK_APP), config);
+        addStaticContent(httpServer);
+        return httpServer;
+    }
+
+    private static void addStaticContent(HttpServer httpServer) {
+        httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler("src/main/webapp/"));
     }
 
     public static void main(String[] args) throws IOException {
